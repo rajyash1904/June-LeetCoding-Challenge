71571bd1f40dd7798239458840429ae31fd83ae9
Yashraj Balidani <ybalidani19.yr@gmail.com>
2020-06-11 16:09:04 +0530

    Create Day11. SortColors.cpp

---
+++ Week 2/Day11. SortColors.cpp
@@ -0,0 +1,52 @@
+/*
+
+Sort Colors
+
+Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+Note: You are not suppose to use the library's sort function for this problem.
+
+Example:
+
+Input: [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Follow up:
+
+1. A rather straight forward solution is a two-pass algorithm using counting sort.
+   First, iterate the array counting number of 0's, 1's, and 2's, then overwrite 
+   array with total number of 0's, then 1's and followed by 2's.
+2. Could you come up with a one-pass algorithm using only constant space?
+
+*/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int l=0;
+        int mid=0;
+        int r=n-1;
+        
+        while(mid<=r)
+        {
+            switch(nums[mid])
+            {
+                case 0:
+                    swap(nums[l],nums[mid]);
+                    l++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(nums[mid],nums[r]);
+                    r--;
+            }
+        }
+        return;
+    }
+};
